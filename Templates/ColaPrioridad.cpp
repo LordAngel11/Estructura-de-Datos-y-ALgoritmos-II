b3763a5bf2e4fb799d8e938fe315708844745120
LordAngel11 <juan.lopez@cimat.mx>
2023-01-23 10:34:59 -0600

    Update del template cola

--- Templates/ColaPrioridad.cpp
+++ Templates/ColaPrioridad.cpp
@@ -1,1 +1,43 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+/*Recordar que un heap es una estructura de datos lineal que 
+se puede visualizar como un arbol binario casi completos
+*/
+template<typename T> void print_queue(T& q){
+         while(!q.empty()){
+            std::cout<< q.top() << "";
+            q.pop();
+         }
+         std::cout<< '\n';   
+        }
+
+    int main(){
+
+        std::priority_queue<int> q; 
+
+        for(int n: {1,8,5,6,3,4,0,9,7,2})
+            q.push(n);
+
+        print_queue(q);
+
+        std::priority_queue<int,std::vector<int>, std::greater<int> > q2;
+
+        for(int n: {1,8,5,6,3,4,0,9,7,2})
+            q2.push(n);
+
+            print_queue(q2);
+
+
+            //Using Lambda to compare elements.
+
+        auto cmp = [](int left, int right) { return (left ^ 1) < (right ^ 1);};
+
+        std::priority_queue<int, std::vector<int>, decltype(cmp) > q3(cmp);
+
+        for(int n: {1,8,5,6,3,4,0,9,7,2})
+            q3.push(n);
+
+        print_queue(q3);
+
+        return 0;
+    }
